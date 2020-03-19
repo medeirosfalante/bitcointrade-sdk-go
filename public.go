694a2f8fc21cc4d755e1a746f16cd329d9234cf8
Rafael Medeiros <rafaelgurudesign@gmail.com>
2020-03-19 02:09:54 -0300

    add new client core

--- public.go
+++ public.go
@@ -2,40 +2,109 @@ package bitcointrade
 
 import (
 	"fmt"
-	"log"
 )
 
 // Public struct
 type Public struct {
+	client *APIClient
 	Crypto string
 }
 
+//Ticker - return a ticket exchange
+type Ticker struct {
+	High     float32 `json:"high"`
+	Low      float32 `json:"low"`
+	Volume   float64 `json:"volume"`
+	Quantity int32   `json:"quantity"`
+	Last     float32 `json:"last"`
+	Buy      float32 `json:"buy"`
+	Sell     float32 `json:"sell"`
+	Date     string  `json:"date"`
+}
+
+//Order - Struct for order
+type Order struct {
+	UnitPrice float32 `json:"unit_price"`
+	Code      string  `json:"code"`
+	Amount    float64 `json:"amount"`
+}
+
+//Orders - Struct for all list order
+type Orders struct {
+	Bids []Order `json:"bids"`
+	Asks []Order `json:"asks"`
+}
+
+//TraderPagination - pagination in trade
+type TraderPagination struct {
+	CurrentPage    int32 `json:"current_page"`
+	PageSize       int32 `json:"page_size"`
+	RegistersCount int32 `json:"registers_count"`
+	TotalPages     int32 `json:"total_pages"`
+}
+
+//Trader - struct for trade
+type Trader struct {
+	Type             string  `json:"type"`
+	Amount           float64 `json:"amount"`
+	UnitPrice        float32 `json:"unit_price"`
+	ActiveOrderCode  string  `json:"active_order_code"`
+	PassiveOrderCode string  `json:"passive_order_code"`
+	Date             string  `json:"date"`
+}
+
+//Traders - Trader response
+type Traders struct {
+	Pagination TraderPagination `json:"pagination"`
+	Trades     []Trader         `json:"trades"`
+}
+
+//TradeQuery - trade query
+type TradeQuery struct {
+	PageSize    int32 `json:"page_size"`
+	CurrentPage int32 `json:"current_page"`
+}
+
+//Public - Create a new instance struct
+func (c *APIClient) Public() *Public {
+	return &Public{client: c}
+}
+
 // Ticker in exchange
-func (p Public) Ticker() (*Response, error) {
-	webClient := WebClient{}
-	resp, err := webClient.GET(fmt.Sprintf("public/%s/ticker", p.Crypto), nil)
+func (p Public) Ticker(pair string) (*Ticker, *Error, error) {
+	var response *Ticker
+	err, errAPI := p.client.Request("GET", fmt.Sprintf("public/%s/ticker", pair), nil, nil, &response)
 	if err != nil {
-		log.Printf("error %s", err)
+		return nil, nil, err
 	}
-	return resp, err
+	if errAPI != nil {
+		return nil, errAPI, nil
+	}
+	return response, nil, nil
 }
 
-// Orders in exchange
-func (p Public) Orders() (*Response, error) {
-	webClient := WebClient{}
-	resp, err := webClient.GET(fmt.Sprintf("public/%s/orders", p.Crypto), nil)
+// Orders - Orders in exchange
+func (p Public) Orders(pair string) (*Orders, *Error, error) {
+	var response *Orders
+	err, errAPI := p.client.Request("GET", fmt.Sprintf("public/%s/orders", pair), nil, nil, &response)
 	if err != nil {
-		log.Printf("error %s", err)
+		return nil, nil, err
 	}
-	return resp, err
+	if errAPI != nil {
+		return nil, errAPI, nil
+	}
+	return response, nil, nil
 }
 
-// Trades in exchange
-func (p Public) Trades() (*Response, error) {
-	webClient := WebClient{}
-	resp, err := webClient.GET(fmt.Sprintf("public/%s/trades", p.Crypto), nil)
+// Trades - Trades in exchange
+func (p Public) Trades(pair string) (*Traders, *Error, error) {
+	var response *Traders
+	err, errAPI := p.client.Request("GET", fmt.Sprintf("public/%s/trades", pair), nil, &TradeQuery{PageSize: 10, CurrentPage: 1}, &response)
 	if err != nil {
-		log.Printf("error %s", err)
+		return nil, nil, err
 	}
-	return resp, err
+	if errAPI != nil {
+		return nil, errAPI, nil
+	}
+	return response, nil, nil
 }
--- public_test.go
+++ public_test.go
@@ -1,47 +1,63 @@
 package bitcointrade_test
 
 import (
-	"bitcointrade"
+	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/rafaeltokyo/bitcointrade-sdk-go"
 )
 
 func TestTicket(t *testing.T) {
-	publicService := &bitcointrade.Public{
-		Crypto: "BTC",
-	}
-	resp, err := publicService.Ticker()
+	godotenv.Load()
+	client := bitcointrade.New("", os.Getenv("ENV"))
+	response, errAPI, err := client.Public().Ticker("BRLBTC")
 	if err != nil {
-		t.Errorf("TestTicket => '%s'", err)
+		t.Errorf("err : %s", err)
 		return
 	}
-	if resp.Search("data") == nil {
-		t.Error("TestTicket => 'data is null erro'")
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if response == nil {
+		t.Error("response is null")
+		return
 	}
 }
 
 func TestOrders(t *testing.T) {
-	publicService := &bitcointrade.Public{
-		Crypto: "BTC",
-	}
-	resp, err := publicService.Orders()
+	godotenv.Load()
+	client := bitcointrade.New("", os.Getenv("ENV"))
+	response, errAPI, err := client.Public().Orders("BRLBTC")
 	if err != nil {
-		t.Errorf("TestOrders => '%s'", err)
+		t.Errorf("err : %s", err)
 		return
 	}
-	if resp.Search("data") == nil {
-		t.Error("TestOrders => 'data is null'")
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if response == nil {
+		t.Error("response is null")
+		return
 	}
 }
+
 func TestTrades(t *testing.T) {
-	publicService := &bitcointrade.Public{
-		Crypto: "BTC",
-	}
-	resp, err := publicService.Trades()
+	godotenv.Load()
+	client := bitcointrade.New("", os.Getenv("ENV"))
+	response, errAPI, err := client.Public().Trades("BRLBTC")
 	if err != nil {
-		t.Errorf("TestTrades => '%s'", err)
+		t.Errorf("err : %s", err)
 		return
 	}
-	if resp.Search("data") == nil {
-		t.Error("TestOrders => 'data is null'")
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if response == nil {
+		t.Error("response is null")
+		return
 	}
 }
